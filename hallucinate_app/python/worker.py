22ad4d24750e553209a5f011e2e327654a0f3ba1
endomorphosis <endomorphosis@users.noreply.github.com>
2024-12-19 18:13:50 -0800

    chore: Update requirements.txt and worker.py for dependency management and cleanup

--- hallucinate_app/python/worker.py
+++ hallucinate_app/python/worker.py
@@ -2,15 +2,12 @@ import os
 # from cloudkit_worker import run
 # import config from .config
 import ipfs_accelerate_py
-import ipfs_agents_py
 import ipfs_kit_py
 import libp2p_kit_py
-import orbitdb_kit_py
 import ipfs_faiss_py 
 import ipfs_model_manager_py
 import ipfs_datasets_py 
 import ipfs_transformers_py 
-import ipfs_agents_py 
 import ipfs_accelerate_py 
 
 class libp2pWorker:
@@ -20,14 +17,12 @@ class libp2pWorker:
         self.resources = resources
         self.metadata = metadata
         self.ipfs = ipfs_kit_py.ipfs_kit(self.resources, self.metadata)
-        self.orbitdb = orbitdb_kit_py.orbitdb_kit(self.resources, self.metadata)
         self.faiss = ipfs_faiss_py.ipfs_faiss_dataset(self.resources, self.metadata)
         self.model_manager = ipfs_model_manager_py.ipfs_model_manager(self.resources, self.metadata)
         self.datasets = ipfs_datasets_py.ipfs_datasets(self.resources, self.metadata)
         self.transformers = ipfs_transformers_py.ipfs_transformers(self.resources, self.metadata)
         self.accelerate = ipfs_accelerate_py.ipfs_accelerate(self.resources, self.metadata)
         self.libp2p = libp2p_kit_py.libp2p_kit(self.resources, self.metadata)
-        self.agents = ipfs_agents_py.ipfs_agent(self.resources, self.metadata)
 
     def init(self, imports):
           if not isinstance(imports, object):
